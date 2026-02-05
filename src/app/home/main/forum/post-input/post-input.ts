eae9b08a5af825850fefc5a2acf61ac2ef22d536
Majka1012 <maja.andrysik@gmail.com>
2026-02-05 23:42:00 +0100

    Revert "update"
    This reverts commit 8b46690f02aa7406f7367bdd888bfd249c875c88.

--- src/app/home/main/forum/post-input/post-input.ts
+++ src/app/home/main/forum/post-input/post-input.ts
@@ -4,55 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-input',
-  imports: [EmojiPicker, FormsModule],
+  imports: [],
   templateUrl: './post-input.html',
   styleUrl: './post-input.css',
 })
-export class PostInput {
-  text = '';
-  showEmojiPicker = false;
-  onEmojiPicked(emoji: string) {
-    this.text += emoji;
-    // console.log(this.text);
-  }
-  lat = 0;
-  lng = 0;
-
-  onLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          if (position) {
-            console.log(position.coords);
-            this.lat = position.coords.latitude;
-            this.lng = position.coords.longitude;
-            this.text += 'Latitude: ' + this.lat + 'Longitude: ' + this.lng;
-            //AIzaSyAY2p8N665quHo0a0pwwO0_RVhXZBD943Q
-          }
-        },
-        (error) => {
-          console.log(error);
-        },
-      );
-    } else {
-      alert('Geolocation is not supported by this browser.');
-    }
-  }
-
-  @ViewChild('emojiContainer') emojiContainer!: ElementRef;
-
-  emojiSelector(event: MouseEvent) {
-    event.stopPropagation();
-    this.showEmojiPicker = true;
-  }
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent) {
-    if (!this.showEmojiPicker) return;
-
-    const clickedInside = this.emojiContainer?.nativeElement.contains(event.target);
-
-    if (!clickedInside) {
-      this.showEmojiPicker = false;
-    }
-  }
-}
+export class PostInput {}
