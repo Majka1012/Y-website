2ca34c101350b75d36a981b3d4e19031aa4d987d
Majka1012 <maja.andrysik@gmail.com>
2026-02-10 15:58:51 +0100

    backend for user

---
+++ backend/src/models/User.js
@@ -0,0 +1,95 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+
+    userHandle: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Podaj prawidłowy email"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    bio: {
+      type: String,
+      maxlength: 200,
+      default: "",
+    },
+    avatarUrl: {
+      type: String,
+      default: "userIcon.png",
+    },
+
+    followersCount: {
+      type: Number,
+      default: 0,
+    },
+
+    followingCount: {
+      type: Number,
+      default: 0,
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    bookmarkedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+
+    likedPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+
+  {
+    timestamps: true,
+  },
+);
+
+schema.pre("save", async function (next) {
+  await doStuff();
+  if (!this.isModified("password")) return next();
+  const bcrypt = require("bcrypt");
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
