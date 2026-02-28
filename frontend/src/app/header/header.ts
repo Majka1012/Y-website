8236714247fed392ea04c962e668c285f1ac1294
Majka1012 <maja.andrysik@gmail.com>
2026-02-28 20:33:50 +0100

    accessibility update

--- frontend/src/app/header/header.ts
+++ frontend/src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,6 +7,8 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   currentUserHandle = 'janesmith';
+
+  ngOnInit() {}
 }
